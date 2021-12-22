e2c24dcd2696831eb06f77fe810b5666a285db06
AndreMatos28 <andrealleati@gmail.com>
2021-12-21 23:47:09 -0300

    add promises

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { OfertasService } from '../ofertas.services'
+import { Oferta } from '../shared/oferta.model'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService] //uso dos serviços no Angular
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas: Oferta[] = [];
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {
+    //this.ofertas = this.ofertasService.getOfertas();
+
+    this.ofertasService.getOfertas2()
+      .then(( ofertas: Oferta[] ) => {
+        console.log("Função resolve() foi resolvida depois de 3 segundos")
+        this.ofertas = ofertas})
+
+
+    .catch(( param: any) => console.log(param));
+  }
+}
